b8efe4f5b2cd94db1356db1d3b19de93f0de87b4
MICKEY935974 <69594578+MICKEY935974@users.noreply.github.com>
2021-12-23 16:54:00 +0530

    Add files via upload

---
+++ Greatest_number.java
@@ -0,0 +1,29 @@
+
+public class Greatest_number {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=500;
+		int c=300;
+		
+		if(a>=b) {
+			if(a>=c)
+			System.out.println("The largest number is="+a);
+			else
+				System.out.println("The largest number is="+c);
+			
+		}
+		else
+		{
+			if(b>=c)
+				System.out.println("The largest number is="+b);
+				else
+					System.out.println("The largest number is="+c);
+					
+		}
+		
+	
+
+	}
+
+}
